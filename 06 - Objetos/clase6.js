faaf5816c3aec544c71b0a56717fe167967c61ea
elGnomo <efrainantonio@ideashappy.com>
2018-09-20 22:21:49 -0500

    Objetos

---
+++ 06 - Objetos/clase6.js
@@ -0,0 +1,34 @@
+"use strict"
+var efrain = {
+    nombre: "Efrain",
+    apellido: "Antonio",
+    edad: 32,
+}
+
+var marvin = {
+    nombre: "Marvin",
+    apellido: "Huerta",
+    edad: 32,
+}
+/*var nombre1 = "Efrain";
+var nombre2 = "Dario";*/
+
+/*function imprimirNombreEnMayusculas(persona) {
+    let nombre = persona.nombre.toUpperCase();
+    console.log("Nombre: ", nombre);
+}*/
+
+function imprimirNombreEnMayusculas({ //llaves y los atributos que nos interesan
+    nombre
+}) {
+    console.log(nombre.toUpperCase());
+}
+
+/*imprimirNombreEnMayusculas(efrain.nombre);
+imprimirNombreEnMayusculas(marvin.nombre);*/
+
+imprimirNombreEnMayusculas(efrain);
+imprimirNombreEnMayusculas(marvin);
+imprimirNombreEnMayusculas({
+    nombre: "Gnomito"
+});
