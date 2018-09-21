355c1bc4c7d9b1cdb6c121ea222dac758ca9c448
elGnomo <efrainantonio@ideashappy.com>
2018-09-21 14:30:04 -0500

    For

---
+++ 13 - For/clase13.js
@@ -0,0 +1,29 @@
+var persona = {
+    nombre: 'efrain',
+    apellido: 'antonio',
+    edad: '32',
+    peso: '89'
+};
+
+console.log(`Al inicio del año ${persona.nombre} pesa ${persona.peso}kg`);
+
+// function aumentarDePeso (persona) {
+//   return persona.peso += 200
+// }
+const INCREMENTO_PESO = 0.2;
+const DIAS_DEL_ANO = 365;
+
+const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO;
+const adelgazar = persona => persona.peso -= INCREMENTO_PESO;
+
+for (var i = 1; i <= DIAS_DEL_ANO; i++) {
+    var random = Math.random()
+
+    if (random < 0.25) {
+        aumentarDePeso(persona)
+    } else if (random < 0.5) {
+        adelgazar(persona)
+    }
+};
+
+console.log(`Al final del año ${persona.nombre} pesa ${persona.peso.toFixed(1)}kg`);
