7c171ba1e6b110cde8b8d858818521a856d3ff5b
elGnomo <efrainantonio@ideashappy.com>
2018-09-20 22:06:02 -0500

    Alcance de las Funciones

---
+++ 05 - Alcance de las Funciones/clase5.js
@@ -0,0 +1,14 @@
+"use strict"
+var nombre = "Efrain";
+
+// function imprimirNombreEnMayusculas() {
+//     nombre = nombre.toUpperCase;
+//     console.log("Nombre: ", nombre);
+// }
+
+function imprimirNombreEnMayusculas(nombre) {
+    nombre = nombre.toUpperCase();
+    console.log("Nombre: ", nombre);
+}
+
+imprimirNombreEnMayusculas(nombre);
